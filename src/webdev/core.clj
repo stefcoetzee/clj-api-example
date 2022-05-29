7f3f13996dddfc0ee6e5cfecf8768db04ff91d4a
Stef Coetzee <stefcoetzee@protonmail.com>
2022-05-29 08:51:41 +0200

    Add more routes, incl. /yo

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,9 +1,15 @@
 (ns webdev.core
-  (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.reload :refer [wrap-reload]]
-            [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found]])
+  (:require [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]
+            [ring.adapter.jetty :as jetty]
+            [ring.handler.dump :refer [handle-dump]]
+            [ring.middleware.reload :refer [wrap-reload]])
   (:gen-class))
+
+(defn about [_]
+  {:status 200
+   :body "Hi! I'm Stef. This is a dummy app for learning Clojure web dev."
+   :headers {}})
 
 (defn greet [_]
   {:status 200
@@ -15,9 +21,22 @@
    :body "Goodbye, Cruel World!"
    :headers {}})
 
+;; (defn request [req]
+;;   {:status 200
+;;    :body (pr-str req)
+;;    :headers {}})
+
+(defn yo [req]
+  {:status 200
+   :body (str "Yo! " (:name (:route-params req)) "!")
+   :headers {}})
+
 (defroutes app
   (GET "/" [] greet)
+  (GET "/about" [] about)
   (GET "/goodbye" [] goodbye)
+  (GET "/request" [] handle-dump)
+  (GET "/yo/:name" [] yo)
   (not-found "Page not found."))
 
 (defn -main [port]
