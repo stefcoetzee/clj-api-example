2d8717f4af32d2e8fd59cd6ea2ff12ad8262e414
Stef Coetzee <stefcoetzee@protonmail.com>
2022-05-29 09:18:02 +0200

    Add calc route

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -11,6 +11,25 @@
    :body "Hi! I'm Stef. This is a dummy app for learning Clojure web dev."
    :headers {}})
 
+(def ops
+  {"+" +
+   "-" -
+   "*" *
+   ":" /})
+
+(defn calc [req]
+  (let [op   (get-in req [:route-params :op])
+        arg1 (Integer. (get-in req [:route-params :arg1]))
+        arg2 (Integer. (get-in req [:route-params :arg2]))
+        f    (get ops op)]
+    (if f
+      {:status  200
+       :body    (str (f arg1 arg2))
+       :headers {}}
+      {:status  404
+       :body    (str "Unknown operator: " op)
+       :headers {}})))
+
 (defn greet [_]
   {:status 200
    :body "Hello, Universe!"
@@ -21,11 +40,6 @@
    :body "Goodbye, Cruel World!"
    :headers {}})
 
-;; (defn request [req]
-;;   {:status 200
-;;    :body (pr-str req)
-;;    :headers {}})
-
 (defn yo [req]
   {:status 200
    :body (str "Yo! " (:name (:route-params req)) "!")
@@ -34,6 +48,7 @@
 (defroutes app
   (GET "/" [] greet)
   (GET "/about" [] about)
+  (GET "/calc/:arg1/:op/:arg2" [] calc)
   (GET "/goodbye" [] goodbye)
   (GET "/request" [] handle-dump)
   (GET "/yo/:name" [] yo)
